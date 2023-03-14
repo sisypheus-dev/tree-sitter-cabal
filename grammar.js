9a4457d8fc4c32227569c6476e3c86958a8fa470
Magnus Therning <magnus@therning.org>
2023-03-14 21:37:42 +0100

    Begin parsing properties

--- grammar.js
+++ grammar.js
@@ -2,7 +2,37 @@ module.exports = grammar({
     name: 'cabal',
 
     rules: {
-        // TODO: add the actual grammar rules
-        source_file: $ => 'hello'
+        cabal: $ => seq(
+            optional($.cabal_version),
+            $.pkg_props
+        ),
+
+        cabal_version: $ => seq(
+            'cabal-version', ':',
+            $.spec_version
+        ),
+
+        spec_version: $ => /\d+\.\d+(\.\d+)?/,
+
+        pkg_props: $ => repeat1($.prop),
+
+        prop: $ => choice(
+            $.prop_version,
+            $.prop_build_type
+        ),
+
+        prop_version: $ => seq(
+            'version', ':',
+            $.pkg_version
+        ),
+
+        pkg_version: $ => /\d+(\.\d+)*/,
+
+        prop_build_type: $ => seq(
+            'build-type', ':',
+            $.build_type_val
+        ),
+
+        build_type_val: $ => choice('Custom', 'Simple')
     }
 });
